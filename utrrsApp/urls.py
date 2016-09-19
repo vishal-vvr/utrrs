5a8538cfe63982c78dcb177e44a7db8d4af8f9fd
tenstormavi <avi.avinash3008@gmail.com>
2016-09-19 15:27:03 +0530

    new urls include

--- utrrsApp/urls.py
+++ utrrsApp/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home')
+    url(r'^$', views.home, name='home'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^history/$', views.history, name='history'),
+    url(r'^roadmap/$', views.roadmap, name='roadmap'),
 ]
