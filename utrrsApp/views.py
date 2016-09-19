bd1bc3a56480f9085d108e45dc8ec6dd06a17b8b
tenstormavi <avi.avinash3008@gmail.com>
2016-09-19 15:29:53 +0530

    new views for html pages included

--- utrrsApp/views.py
+++ utrrsApp/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 
 def home(request):
 	return render(request, 'home.html')
+
+def about(request):
+	return render(request, 'about.html')
+
+def history(request):
+	return render(request, 'history.html')
+
+def roadmap(request):
+	return render(request, 'roadmap.html')
