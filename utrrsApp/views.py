f85848d02c5ddd0d0bb1ad3eb263b92513536ed8
tenstormavi <avi.avinash3008@gmail.com>
2016-09-26 18:17:57 +0530

    add function for fetching data for the page

--- utrrsApp/views.py
+++ utrrsApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import os
 
 def home(request):
 	return render(request, 'home.html')
@@ -14,5 +15,17 @@ def roadmap(request):
 	return render(request, 'roadmap.html')
 
 def hindi(request):
-	return render(request, 'hindi.html')
-
+	module_dir = os.path.dirname(__file__)
+	file_path = os.path.join(module_dir, 'static/lang/hi_IN/font/data/codepoint/master_hi.txt')
+	file = open(file_path)
+	c = file.read()
+	length = c.count('\n')
+	file.close()
+	file = open(file_path)
+	data = []
+	for i in range(length):
+		line = file.readline()
+		st = line.strip('\n')
+		sp = st.split(',')
+		data.append(sp)
+	return render(request, 'test.html', {'data': data[1:]})
