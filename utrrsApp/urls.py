b2670d4c6f411bc1ea18c3c9de1fa5dd56512f34
tenstormavi <avi.avinash3008@gmail.com>
2016-09-27 15:44:15 +0530

    Add URL for different languages

--- utrrsApp/urls.py
+++ utrrsApp/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     url(r'^about/$', views.about, name='about'),
     url(r'^history/$', views.history, name='history'),
     url(r'^roadmap/$', views.roadmap, name='roadmap'),
+    url(r'^assamese/$', views.assamese, name='assamese'),
+    url(r'^bengali/$', views.bengali, name='bengali'),
+    url(r'^german/$', views.german, name='german'),
+    url(r'^gujarati/$', views.gujarati, name='gujarati'),
     url(r'^hindi/$', views.hindi, name='hindi'),
 ]
