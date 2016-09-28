797e05ce61f2daaddc1141ddfd79a66c027c9564
tenstormavi <avi.avinash3008@gmail.com>
2016-09-28 17:51:36 +0530

    New URL for languages

--- utrrsApp/urls.py
+++ utrrsApp/urls.py
@@ -12,4 +12,12 @@ urlpatterns = [
     url(r'^german/$', views.german, name='german'),
     url(r'^gujarati/$', views.gujarati, name='gujarati'),
     url(r'^hindi/$', views.hindi, name='hindi'),
+    url(r'^kannada/$', views.kannada, name='kannada'),
+    url(r'^maithili/$', views.maithili, name='maithili'),
+    url(r'^malayalam/$', views.malayalam, name='malayalam'),
+    url(r'^marathi/$', views.marathi, name='marathi'),
+    url(r'^odia/$', views.odia, name='odia'),
+    url(r'^punjabi/$', views.punjabi, name='punjabi'),
+    url(r'^tamil/$', views.tamil, name='tamil'),
+    url(r'^telugu/$', views.telugu, name='telugu'),
 ]
