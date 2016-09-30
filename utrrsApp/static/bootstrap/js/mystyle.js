a6dbaf1ad96164fb801110af2edbc30ee657f5dd
tenstormavi <avi.avinash3008@gmail.com>
2016-09-30 16:24:58 +0530

    Add back to top button

---
+++ utrrsApp/static/bootstrap/js/mystyle.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    $('#back-to-top').click(function () {
+        $('#back-to-top').tooltip('hide');
+        $('body,html').animate({
+            scrollTop: 0
+        }, 1000);
+        return false;
+    });
+        
+    $('#back-to-top').tooltip('show');
+
+});
