2c84957ccc936c6f2e94beca647c82dabcfae1f2
tenstormavi <avi.avinash3008@gmail.com>
2016-10-04 16:17:20 +0530

    Add function for check fonts

--- utrrsApp/views.py
+++ utrrsApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import os
+import shlex, subprocess
+from collections import Counter
 
 def home(request):
 	return render(request, 'home.html')
@@ -158,6 +160,28 @@ def german(request):
 		data.append(sp)
 	return render(request, 'german.html', {'data': data[1:]})
 
+def german_codepoint(request):
+	module_dir = os.path.dirname(__file__)
+	file_path = os.path.join(module_dir, 'static/lang/de_DE/font/data/codepoint/master_de_DE.txt')
+	file = open(file_path)
+	c = file.read()
+	length = c.count('\n')
+	file.close()
+	file = open(file_path)
+	data = []
+	for i in range(length):
+		line = file.readline()
+		st = line.strip('\n')
+		sp = st.split(',')
+		data.append(sp)
+	return render(request, 'german.html', {'data': data[1:]})
+
+def german_gsub(request):
+	return render(request, 'de_gsub.html')
+
+def german_gpos(request):
+	return render(request, 'de_gpos.html')
+
 def gujarati(request):
 	module_dir = os.path.dirname(__file__)
 	file_path = os.path.join(module_dir, 'static/lang/gu_IN/font/data/codepoint/master_gu_IN.txt')
@@ -174,6 +198,54 @@ def gujarati(request):
 		data.append(sp)
 	return render(request, 'gujarati.html', {'data': data[1:]})
 
+def gujarati_codepoint(request):
+	module_dir = os.path.dirname(__file__)
+	file_path = os.path.join(module_dir, 'static/lang/gu_IN/font/data/codepoint/master_gu_IN.txt')
+	file = open(file_path)
+	c = file.read()
+	length = c.count('\n')
+	file.close()
+	file = open(file_path)
+	data = []
+	for i in range(length):
+		line = file.readline()
+		st = line.strip('\n')
+		sp = st.split(',')
+		data.append(sp)
+	return render(request, 'gujarati.html', {'data': data[1:]})
+
+def gujarati_gsub(request):
+	module_dir = os.path.dirname(__file__)
+	file_path = os.path.join(module_dir, 'static/lang/gu_IN/font/data/gsub/master_gsub_gu_IN.txt')
+	file = open(file_path)
+	c = file.read()
+	length = c.count('\n')
+	file.close()
+	file = open(file_path)
+	data = []
+	for i in range(length):
+		line = file.readline()
+		st = line.strip('\n')
+		sp = st.split(',')
+		data.append(sp)
+	return render(request, 'gu_gsub.html', {'data': data[1:]})
+
+def gujarati_gpos(request):
+	module_dir = os.path.dirname(__file__)
+	file_path = os.path.join(module_dir, 'static/lang/gu_IN/font/data/gpos/master_gpos_gu_IN.txt')
+	file = open(file_path)
+	c = file.read()
+	length = c.count('\n')
+	file.close()
+	file = open(file_path)
+	data = []
+	for i in range(length):
+		line = file.readline()
+		st = line.strip('\n')
+		sp = st.split(',')
+		data.append(sp)
+	return render(request, 'gu_gpos.html', {'data': data[1:]})
+
 def hindi(request):
 	module_dir = os.path.dirname(__file__)
 	file_path = os.path.join(module_dir, 'static/lang/hi_IN/font/data/codepoint/master_hi_IN.txt')
@@ -189,6 +261,54 @@ def hindi(request):
 		sp = st.split(',')
 		data.append(sp)
 	return render(request, 'hindi.html', {'data': data[1:]})
+
+def hindi_codepoint(request):
+	module_dir = os.path.dirname(__file__)
+	file_path = os.path.join(module_dir, 'static/lang/hi_IN/font/data/codepoint/master_hi_IN.txt')
+	file = open(file_path)
+	c = file.read()
+	length = c.count('\n')
+	file.close()
+	file = open(file_path)
+	data = []
+	for i in range(length):
+		line = file.readline()
+		st = line.strip('\n')
+		sp = st.split(',')
+		data.append(sp)
+	return render(request, 'hindi.html', {'data': data[1:]})
+
+def hindi_gsub(request):
+	module_dir = os.path.dirname(__file__)
+	file_path = os.path.join(module_dir, 'static/lang/hi_IN/font/data/gsub/master_gsub_hi_IN.txt')
+	file = open(file_path)
+	c = file.read()
+	length = c.count('\n')
+	file.close()
+	file = open(file_path)
+	data = []
+	for i in range(length):
+		line = file.readline()
+		st = line.strip('\n')
+		sp = st.split(',')
+		data.append(sp)
+	return render(request, 'hi_gsub.html', {'data': data[1:]})
+
+def hindi_gpos(request):
+	module_dir = os.path.dirname(__file__)
+	file_path = os.path.join(module_dir, 'static/lang/hi_IN/font/data/gpos/master_gpos_hi_IN.txt')
+	file = open(file_path)
+	c = file.read()
+	length = c.count('\n')
+	file.close()
+	file = open(file_path)
+	data = []
+	for i in range(length):
+		line = file.readline()
+		st = line.strip('\n')
+		sp = st.split(',')
+		data.append(sp)
+	return render(request, 'hi_gpos.html', {'data': data[1:]})
 
 def kannada(request):
 	module_dir = os.path.dirname(__file__)
@@ -317,3 +437,10 @@ def telugu(request):
 		sp = st.split(',')
 		data.append(sp)
 	return render(request, 'telugu.html', {'data': data[1:]})
+
+def check_fonts():
+	command = 'fc-match sans-serif:lang=te'
+	command = shlex.split(c)
+	res = subprocess.check_output(x)
+	Counter(z) == Counter(q)
+
