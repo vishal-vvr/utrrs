cf446b0a0d7c5d092d8f9a6595220c1bd024e1f1
tenstormavi <avi.avinash3008@gmail.com>
2016-10-04 16:16:01 +0530

    Add float window for check fonts

--- utrrsApp/static/bootstrap/js/mystyle.js
+++ utrrsApp/static/bootstrap/js/mystyle.js
@@ -13,3 +13,12 @@ $(document).ready(function(){
         return false;
     });
 });
+
+$(".modal-transparent").on('show.bs.modal', function () {
+  setTimeout( function() {
+    $(".modal-backdrop").addClass("modal-backdrop-transparent");
+  }, 0);
+});
+$(".modal-transparent").on('hidden.bs.modal', function () {
+  $(".modal-backdrop").addClass("modal-backdrop-transparent");
+});
